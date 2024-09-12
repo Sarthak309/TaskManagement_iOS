1732843f9e9deeb594590bf83cf3872ca0181033
Sarthak Agrawal <agrawal.sarthak87@gmail.com>
2024-09-12 14:55:28 +0530

    Initial Commit

---
+++ TaskManagementApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManagementApplication
+//
+//  Created by Sarthak Agrawal on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagementApplication/TaskManagementApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApplicationApp.swift
+//  TaskManagementApplication
+//
+//  Created by Sarthak Agrawal on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
